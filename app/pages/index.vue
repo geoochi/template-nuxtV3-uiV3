62dc2709d2aa9279abcf8d41642a615014833c9a
Geoo Chi <geoochi@geoochi.com>
2025-04-03 15:27:01 +0800

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
 const colorMode = useColorMode()
-const isDark = computed({
-  get() {
-    return colorMode.value === 'dark'
-  },
-  set() {
-    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-  },
-})
 </script>
 
 <template>
   <title>template - nuxt@3 - nuxt/ui@3</title>
   <p class="text-3xl">template - nuxt@3 - nuxt/ui@3</p>
   <UButton
-    :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+    :icon="$colorMode.value === 'dark' ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
     variant="ghost"
-    @click="isDark = !isDark"
+    @click="colorMode.value = colorMode.value === 'dark' ? 'light' : 'dark'"
   />
   <NuxtLink to="/about">Link to About</NuxtLink>
   <UButton to="/about">Button to About</UButton>
