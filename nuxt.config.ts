10969f5089673c4ba6dc3378fba5fb47200ec488
Geoo Chi <geoochi@geoochi.com>
2025-04-03 14:37:16 +0800

    add compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['./app/app.css'],
+  compatibilityDate: '2025-04-03',
   devtools: { enabled: false },
   fonts: { provider: 'local' },
   future: { compatibilityVersion: 4 },
