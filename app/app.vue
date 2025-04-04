7398c178904fb65d6463321658d0e46ebfeb1637
Geoo Chi <geoochi@geoochi.com>
2025-04-04 15:48:27 +0800

    update

--- app/app.vue
+++ app/app.vue
@@ -9,18 +9,32 @@ const togglePreference = () => {
 const toggleValue = () => {
   colorMode.value = colorMode.value === 'dark' ? 'light' : 'dark'
 }
-let colorValue = ref('')
-onMounted(() => {
-  colorValue = computed(() => colorMode.value)
-})
 </script>
 <template>
   <UApp>
     <div class="flex flex-col items-center justify-center h-screen gap-4">
       <p>{{ colorMode }}</p>
       <div class="flex gap-4">
-        <UButton variant="outline" :icon="colorMode.preference === 'light' ? 'i-lucide-sun' : 'i-lucide-moon'" />
-        <UButton variant="outline" :icon="colorValue === 'light' ? 'i-lucide-sun' : 'i-lucide-moon'" />
+        <UButton
+          variant="outline"
+          :icon="
+            colorMode.preference === 'system'
+              ? 'i-lucide-loader-circle'
+              : colorMode.preference === 'light'
+              ? 'i-lucide-sun'
+              : 'i-lucide-moon'
+          "
+        />
+        <UButton
+          variant="outline"
+          :icon="
+            colorMode.value === 'system'
+              ? 'i-lucide-loader-circle'
+              : colorMode.value === 'light'
+              ? 'i-lucide-sun'
+              : 'i-lucide-moon'
+          "
+        />
       </div>
       <div class="flex gap-4">
         <UButton @click="togglePreference">Toggle Preference</UButton>
