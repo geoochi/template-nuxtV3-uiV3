812b22f792f09d5733979921ecdbe00430d12834
Geoo Chi <geoochi@geoochi.com>
2025-04-06 20:13:24 +0800

    use github deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/template-nuxtV3-uiV3/',
+  },
   compatibilityDate: '2025-04-03',
   css: ['./app/global.css'],
   devtools: { enabled: false },
